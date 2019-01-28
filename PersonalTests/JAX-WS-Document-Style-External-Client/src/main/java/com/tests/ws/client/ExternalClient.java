bd77e064b689d5dd2d9a8d081b156a5144455869
Utente Hp <Utente Hp@UtenteHp-PC>
2019-01-28 16:34:43 +0100

    Jaxws

--- PersonalTests/JAX-WS-Document-Style-External-Client/src/main/java/com/tests/ws/client/ExternalClient.java
+++ PersonalTests/JAX-WS-Document-Style-External-Client/src/main/java/com/tests/ws/client/ExternalClient.java
@@ -1,6 +1,12 @@
 package com.tests.ws.client;
 
 import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
+import com.tests.jaxws.ws.HelloWorld;
 
 
 public class ExternalClient {
@@ -8,11 +14,12 @@ public class ExternalClient {
 	public String getHelloWorldCustomMsg(String name) throws MalformedURLException {
 		String ret = null;
 		
-//		URL url = new URL("http://localhost:9999/ws/hello?wsdl");
-//	    QName qname = new QName("http://ws.jaxws.tests.com/", "HelloWorldImplService");
-//        Service service = Service.create(url, qname);
-//        HelloWorld hello = service.getPort(HelloWorld.class);
-//        ret = hello.getHelloWorldAsString(name);
+		URL url = new URL("http://localhost:9999/ws/hello?wsdl");
+	    QName qname = new QName("http://ws.jaxws.tests.com/", "HelloWorldImplService");
+        Service service = Service.create(url, qname);
+        
+        HelloWorld hello = service.getPort(HelloWorld.class);
+        ret = hello.getHelloWorldAsString(name);
         
         return ret;
 	}
